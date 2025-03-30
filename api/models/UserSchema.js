0e296ca3c1ef3aa0ffa6170939e53bc0e4b31e65
Johnalex32 <jbmorelo132@gmail.com>
2025-03-30 16:20:31 -0500

    Update UserSchema.js

--- api/models/UserSchema.js
+++ api/models/UserSchema.js
@@ -1,35 +1,18 @@
-import mongoose from "mongoose";
-import bcrypt from "bcrypt"
+import mongoose from 'mongoose';
 
-
-const userSchema = mongoose.Schema({ 
-   name:{
-    type: String,
-    require: true,
-    trim:true
-   },
-   password:{
-    type: String,
-    require: true,
-    trim:true
-   }
+const userSchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatorio'],
+        trim: true,
+        minlength: [3, 'El nombre debe tener al menos 3 caracteres'],
+        maxlength: [50, 'El nombre no puede tener más de 50 caracteres']
+    },
+    numeroMesa: {
+        type: Number,
+        required: [true, 'El número de mesa es obligatorio'],
+        min: [1, 'El número de mesa debe ser mayor a 0']
+    },
 });
 
-userSchema.pre('save',async function (next) {
-    if (!this.isModified('password')) {
-        next();
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    
-});
-
-userSchema.methods.checkPassword = async function (passwordForm) {
-    return await bcrypt.compare(passwordForm, this.password);
-    
-}
-
-const User = mongoose.model('User', userSchema);
-
-export default User;
+export default mongoose.model('User', userSchema);
